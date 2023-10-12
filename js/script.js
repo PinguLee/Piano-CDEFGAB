bcf45c357d757adc385ced409b8f407fe889347d
leemingoo <dwmapi@naver.com>
2023-10-12 17:58:56 +0900

    음악 파일 배열 처리 후 클릭 이벤트 받아 실행

--- js/script.js
+++ js/script.js
@@ -1,20 +1,21 @@
+const root = document.getElementById('root');
 const canvas = document.getElementById('piano');
 const ctx = canvas.getContext('2d');
+const audio = root.querySelectorAll('audio');
 
-let arr = ["red","orange","yellow","green","blue","navy","violet"];
-let arr1 = ["도","레","미","파","솔","라","시"];
+const arr = ["red", "orange", "yellow", "green", "blue", "navy", "violet"];
+const arr1 = ["도", "레", "미", "파", "솔", "라", "시"];
 
-for ( let i = 0; i < 7; i++){
+for (let i = 0; i < 7; i++) {
     ctx.fillStyle = arr[i]
-    ctx.fillRect(i*100, 0, 100,500);
+    ctx.fillRect(i * 100, 0, 100, 500);
 }
 
-
-
-canvas.addEventListener('click',(e)=>{
-    let clickX = e.clientX-ctx.canvas.offsetLeft;
-    for( let i = 0 ; i < arr1.length ; i++){
-        if(i < clickX && clickX < (i+1)*100){
+canvas.addEventListener('click', (e) => {
+    let clickX = e.clientX - ctx.canvas.offsetLeft;
+    for (let i = 0; i < arr1.length; i++) {
+        if (i < clickX && clickX < (i + 1) * 100) {
+            audio[i].play()
             console.log(arr1[i]);
             return;
         }
