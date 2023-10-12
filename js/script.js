99f1d1565ed60cb8911c9e0d7cda1d081759a4bc
최은철 <wndqhrgkdlel@gmail.com>
2023-10-12 17:36:36 +0900

    클릭 시 해당 음계 출력

--- js/script.js
+++ js/script.js
@@ -1,10 +1,22 @@
 const canvas = document.getElementById('piano');
 const ctx = canvas.getContext('2d');
 
+let arr = ["red","orange","yellow","green","blue","navy","violet"];
+let arr1 = ["도","레","미","파","솔","라","시"];
+
 for ( let i = 0; i < 7; i++){
-    ctx.fillRect(i*50, 50, 50, 300);
+    ctx.fillStyle = arr[i]
+    ctx.fillRect(i*100, 0, 100,500);
 }
 
-ctx.addEventListener('click',(e)=>{
-    console.log(e);
+
+
+canvas.addEventListener('click',(e)=>{
+    let clickX = e.clientX-ctx.canvas.offsetLeft;
+    for( let i = 0 ; i < arr1.length ; i++){
+        if(i < clickX && clickX < (i+1)*100){
+            console.log(arr1[i]);
+            return;
+        }
+    }
 })
