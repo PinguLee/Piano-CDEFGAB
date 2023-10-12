b2451fb304e40b45c95138082eb17746bb324c52
최은철 <wndqhrgkdlel@gmail.com>
2023-10-12 16:46:10 +0900

    canvas 클릭 이벤트

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
-const root = document.getElementById('root');
-const piano = document.getElementById('piano');
-const context = piano.getContext('2d');
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+for ( let i = 0; i < 7; i++){
+    ctx.fillRect(i*50, 50, 50, 300);
+}
+
+canvas.addEventListener('click',(e)=>{
+    console.log(e);
+})
